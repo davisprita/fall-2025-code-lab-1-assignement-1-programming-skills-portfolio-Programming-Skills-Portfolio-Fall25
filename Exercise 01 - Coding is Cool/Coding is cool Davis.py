aa5cd5e30a2cb1f8ddbdb7d2599ecdccaa95a375
davisprita <davis.p.u10@gmail.com>
2025-10-24 12:38:54 +0400

    Create Coding is cool Davis.py

---
+++ Exercise 01 - Coding is Cool/Coding is cool Davis.py
@@ -0,0 +1,7 @@
+#Coding is cool 
+#Creating Variables
+Sunday="Coding "
+Monday="is "
+Tuesday="Cool"
+#Using string concatenation to combine the variables and print them
+print( Sunday + Monday +Tuesday )
