ab12943864a04d27e54c07a64861492f81409135
davisprita <davis.p.u10@gmail.com>
2025-11-24 22:15:31 +0400

    Some Counting
    In this exercise, we use the loop function to count from 0 to 50 in increments of 1, 50 to 0 in decrements of 1, 30 to 50 in increments of 1, 50 to 10 in decrements of 2, and 100 to 200 in increments of 5

---
+++ Exercise 07 - Some Counting/Some Counting Davis.py
@@ -0,0 +1,26 @@
+#Some Counting
+#1. Loop that counts from 0 to 50 in the increments of 1
+print("Counting from 0 to 50:-")
+for d in range(0,51,1):
+    print(d)
+print('-----------------------------------------------------------------') #For separation 
+#2. Loop that counts down from 50 to 0 in the decrements of 1
+print("Counting down from 50 to 0:-")
+for a in range(50,-1,-1):
+    print(a)
+print('-----------------------------------------------------------------') #For separation
+#3. Loop that counts from 30 to 50 in the increments of 1
+print("Counting from 30 to 50:-")
+for v in range(30,51,1):
+    print(v)
+print('-----------------------------------------------------------------') #For separation
+#4. Loop that counts down from 50 to 10 decrements of 2
+print("Counting down from 50 to 10:-")
+for i in range(50,9,-2):
+    print(i)
+print('-----------------------------------------------------------------') #For separation
+#4. Loop that counts from 100 to 200 in the increments of 5
+print("Counting from 100 to 200:-")
+for s in range(100,201,5):
+    print(s)
+print('-----------------------------------------------------------------') #For separation
