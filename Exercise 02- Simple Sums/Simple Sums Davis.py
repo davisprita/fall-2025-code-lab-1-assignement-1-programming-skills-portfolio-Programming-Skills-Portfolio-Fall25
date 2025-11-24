7093e0bf1cefd566dc08132fd69e06d9b1c2ea83
davisprita <davis.p.u10@gmail.com>
2025-11-24 21:51:31 +0400

    Simple Sums
    In this exercise, we have variables that store integers, and with them, we perform basic arithmetic, and then print/display the final result

--- Exercise 02- Simple Sums/Simple Sums Davis.py
+++ Exercise 02- Simple Sums/Simple Sums Davis.py
@@ -2,6 +2,8 @@
 #Creating Variables 
 vary1=8
 vary2=10
+vary1=8 #1st Variable
+vary2=10 #2nd Variable
 #Creating 3rd Variable which contains the sum of the above to variables 
 sum=vary1+vary2
 #Printing the sum 
