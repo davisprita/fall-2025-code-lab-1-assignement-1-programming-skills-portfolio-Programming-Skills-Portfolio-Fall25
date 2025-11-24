24a1f3a4c6c16f3d9167e28b909cd395b3de76f2
davisprita <davis.p.u10@gmail.com>
2025-11-24 21:45:35 +0400

    Coding is cool
    In this Exercise, the output coding is cool is displayed with the help of variables and concatenation

--- Exercise 01 - Coding is Cool/Coding is cool Davis.py
+++ Exercise 01 - Coding is Cool/Coding is cool Davis.py
@@ -3,5 +3,9 @@
 Sunday="Coding "
 Monday="is "
 Tuesday="Cool"
+Sunday="Coding " #1st Variable
+Monday="is " #2nd Variable
+Tuesday="Cool" #3rd Variable
 #Using string concatenation to combine the variables and print them
 print( Sunday + Monday +Tuesday )
+print( Sunday+Monday+Tuesday)
